013ee83bd41a894b4a6aacbcb516604cb0779531
summer <tinghung.wang@104.com.tw>
2022-01-28 13:27:03 +0800

    add init express

---
+++ index.js
@@ -0,0 +1,11 @@
+let express = require('express');
+  //1.載入express模組
+let app = express();
+  // 2.使用express
+app.get('/', function (req, res) {
+    res.send('Hello')
+})
+  // 5.首頁
+let port = 3000;
+  //3.設定port位置
+app.listen(port);
