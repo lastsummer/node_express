e8849e55d287aa75c0111ece62ec2ea890777fa0
summer <tinghung.wang@104.com.tw>
2022-02-10 09:48:37 +0800

    新增報表

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer  = require('multer');
 const readExcel = require('./readExcel')
+const wang = require('./wang')
 
 let app = express();
 app.use(express.static('public'));
@@ -27,6 +28,22 @@ app.get('/download/:filename', function(req, res){
 app.get('/wang', function(req, res){
   res.render('wangLogin')
 })
+app.get('/wang/dashboard', async function(req, res){
+  const data = await wang.getCurrentMonth()
+  res.render('wangDashboad',{data})
+})
+app.get('/wang/search', async function(req, res){
+  const data = await wang.getMonthData(req.query.year*1, req.query.month*1-1)
+  res.json({data})
+})
+app.get('/wang/saveTime', async function(req, res){
+  const data = await wang.saveTime(req.query.year*1, req.query.month*1, req.query.day, req.query.start, req.query.end)
+  res.json({data})
+})
+app.get('/wang/deleteTime', async function(req, res){
+  const data = await wang.deleteTime(req.query.year*1, req.query.month*1, req.query.day)
+  res.json({data})
+})
 
-let port = 80;
+let port = 8080;
 app.listen(port);
